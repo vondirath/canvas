74bf5fd0195c69ade572104b38ef2e63217e1a2c
Thomas Lopez <Vondirath@gmail.com>
2017-04-23 10:09:53 -0600

    docs added comments and added star radius to settings

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 /* script */
 var c,
     islandPainting = {
+
+        // app settings, Not split up at this time but can be by defining an additional data structure.
         settings: {
             canvas: document.getElementById("canvasPaint"),
             canvasWidth: 1000,
@@ -10,10 +12,12 @@ var c,
             clouds: ["cloud1.svg", "cloud2.svg", "cloud3.svg", "cloud4.svg", "cloud5.svg"],
             star_count: 100,
             star_colors: ["#3F9275", "#296B59", "#449170"],
-
+            star_radius: 2,
 
 
         },
+
+        // kicks off APP by filling in the pieces one by one.
         init: function () {
             c = this.settings;
             this.drawBackground();
@@ -22,6 +26,7 @@ var c,
             this.drawImg(c.island, 0, 0, c.canvasWidth, c.canvasHeight);
         },
 
+        // draws the radiant background
         drawBackground: function () {
             ctx = c.canvas.getContext("2d");
             grd = ctx.createLinearGradient(0, 0, 600, 0);
@@ -31,14 +36,16 @@ var c,
             ctx.fillRect(0, 0, c.canvasWidth, c.canvasHeight);
         },
 
+        // draws stars by the star_count setting and star_colors
         drawStars: function () {
             ctx = c.canvas.getContext("2d");
             for (var i = 0; i < c.star_count; i++) {
                 var x = Math.random() * c.canvas.offsetWidth,
                 y = Math.random() * c.canvas.offsetHeight,
-                radius = Math.random() * 1.2;
+                radius = Math.random() * c.star_radius;
                 ctx.beginPath();
                 ctx.arc(x, y, radius, 0, 360)
+                
                 ctx.fillStyle = islandPainting.getRandom(c.star_colors)
                 ctx.fill();
             };
@@ -46,6 +53,7 @@ var c,
 
         },
 
+        // draws an image from source at X and Y axis. 
         drawImg: function (source, x, y, w, h) {
             ctx = c.canvas.getContext("2d")
             var img = new Image();
@@ -55,6 +63,7 @@ var c,
             img.src = source;
         },
 
+        // getRandom helper. returns random index from list
         getRandom: function(list) {
             return list[Math.floor(Math.random() * list.length)];
         },
